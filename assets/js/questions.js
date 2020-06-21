de1ade758248e1adae02d30f33166eeb473e8c3f
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-06-21 16:28:06 -0500

    Rudimentary question handling.

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var quizList = [
+	{
+		question: "True or False: Javascript arrays can hold any type of value, even different types within the same array.",
+		choices: [ "True", "False"],
+		correct: 0
+	},
+	{
+		question: "True or False: Javascript is a strongly typed language, meaning all variables must be declared as their data type: int, string, and so on.",
+		choices: [ "True", "False"],
+		correct: 1
+	},
+	{
+		question: "Which of these methods is NOT valid for incrementing a variable called 'index'?",
+		choices: [ "index++;", "index += 1;", "increment index;", "index = index + 1;"],
+		correct: 2
+	}
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
 var startButtonEl = document.querySelector("#startButton");
 var questionEl = document.querySelector("#testQuestion");
+var answerList = questionEl.querySelector("ol");
+var answerStatus = questionEl.querySelector("#answerStatus");
+
+var curQuestion = 0;
+
+var loadNextQuestion = function() {
+	// TODO - Random question!!!
+
+	questionEl.querySelector("h2").textContent = (curQuestion + 1) + ": " + quizList[curQuestion].question;
+
+	answerList.innerHTML = ""; // Destroy all children
+
+	for (var i = 0; i < quizList[curQuestion].choices.length; i++) {
+		var answerEl = document.createElement("li");
+		answerEl.setAttribute("data-answer-id", i);
+		answerEl.textContent = quizList[curQuestion].choices[i];
+		answerList.appendChild(answerEl);
+	}
+
+	answerStatus.textContent = "";
+}
+
+var answerQuestion = function(event) {
+	var correct = (event.target.getAttribute("data-answer-id") == quizList[curQuestion].correct);
+
+	curQuestion++; // Testing code!
+	loadNextQuestion();
+
+	answerStatus.textContent = (correct) ? "Correct" : "Wrong";
+}
 
 var beginTest = function() {
 	document.querySelector("#buttonHolder").style.display = "none";
-	
+
 	questionEl.style.display = "flex";
+
+	loadNextQuestion();
 }
 
 startButtonEl.addEventListener("click", beginTest);
+answerList.addEventListener("click", answerQuestion);
