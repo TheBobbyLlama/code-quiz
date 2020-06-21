5b942754e125329e68f28dd9b57ae3ea2a98f386
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-06-21 18:08:12 -0500

    Implemented test logic flow.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,62 @@
 var quizList = [
 	{
 		question: "True or False: Javascript arrays can hold any type of value, even different types within the same array.",
-		choices: [ "True", "False"],
+		choices: [ "True", "False" ],
 		correct: 0
 	},
 	{
 		question: "True or False: Javascript is a strongly typed language, meaning all variables must be declared as their data type: int, string, and so on.",
-		choices: [ "True", "False"],
+		choices: [ "True", "False" ],
 		correct: 1
 	},
 	{
+		question: "True or False: innerText, innerHTML, and textContent all reference the same part of an HTML element.",
+		choices: [ "True", "False" ],
+		correct: 1
+	},
+	{
+		question: "True or Fale: The structure of Javascript objects is immutable; extra fields cannot be added.",
+		choices: [ "True", "False" ],
+		correct: 1
+	},
+	{
+		question: "True or False: The '===' is for comparing values strictly, while the '==' allows looser comparison instead.",
+		choices: [ "True", "False" ],
+		correct: 0
+	},
+	{
+		question: "True or False: Not only are [square brackets] used to declare arrays, they are also used to access the items inside.",
+		choices: [ "True", "False" ],
+		correct: 0
+	},
+	{
 		question: "Which of these methods is NOT valid for incrementing a variable called 'index'?",
-		choices: [ "index++;", "index += 1;", "increment index;", "index = index + 1;"],
+		choices: [ "index++;", "index += 1;", "increment index;", "index = index + 1;" ],
 		correct: 2
+	},
+	{
+		question: "What is the name of the object used for storing persistent data for a web page?",
+		choices: [ "settingStorage", "cookieManager", "dataBucket", "localStorage" ],
+		correct: 3
+	},
+	{
+		question: "Variables that are declared outside of any functions or objects are known as what kind of variables?",
+		choices: [ "Global", "Super", "Local", "Universal" ],
+		correct: 0
+	},
+	{
+		question: "Which of these methods is the correct way to start a for loop?",
+		choices: [ "for (var i = 0; i < 10)", "for (var i = 0; i < 10; i++)", "for (var i = 1 to 10)", "for (var i = 0; i < 10; i + 1)" ],
+		correct: 1
+	},
+	{
+		question: "We often use the acronym 'DRY' as a best practice.  What does this mean?",
+		choices: [ "Don't Rush Yourself", "Divide, Refactor, Yield", "Don't Repeat Yourself", "Dummies Really Yeet" ],
+		correct: 3
+	},
+	{
+		question: "What is code {enclosed in curly braces} known as?",
+		choices: [ "A block", "A bucket", "Grouped", "Sequestered"],
+		correct: 0
 	}
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
-var startButtonEl = document.querySelector("#startButton");
-var questionEl = document.querySelector("#testQuestion");
-var answerList = questionEl.querySelector("ol");
-var answerStatus = questionEl.querySelector("#answerStatus");
+var contentEl = document.querySelector("#mainContent");
 
-var curQuestion = 0;
+var timeLeft;
+var timer;
+var questionNumber
+var correctAnswers;
+var curQuestion;
+var askedQuestions = [];
+
+var timerTick = function() {
+	timeLeft--;
+
+	if (timeLeft <= 0) {
+		timeLeft = 0;
+		endTest();
+	}
+
+	document.querySelector("#timer").textContent = "Time: " + timeLeft;
+}
 
 var loadNextQuestion = function() {
-	// TODO - Random question!!!
+	questionNumber++;
 
-	questionEl.querySelector("h2").textContent = (curQuestion + 1) + ": " + quizList[curQuestion].question;
+	// Select the next question randomly, but don't allow repeats.
+	do {
+		curQuestion = Math.floor(quizList.length * Math.random());
+	} while (askedQuestions.indexOf(curQuestion) > -1)
 
-	answerList.innerHTML = ""; // Destroy all children
+	askedQuestions.push(curQuestion);
+
+	contentEl.querySelector("h2").textContent = questionNumber + ": " + quizList[curQuestion].question;
+
+	var answerList = contentEl.querySelector("ol");
+	answerList.innerHTML = "";
 
 	for (var i = 0; i < quizList[curQuestion].choices.length; i++) {
 		var answerEl = document.createElement("li");
@@ -18,26 +39,91 @@ var loadNextQuestion = function() {
 		answerEl.textContent = quizList[curQuestion].choices[i];
 		answerList.appendChild(answerEl);
 	}
-
-	answerStatus.textContent = "";
 }
 
 var answerQuestion = function(event) {
-	var correct = (event.target.getAttribute("data-answer-id") == quizList[curQuestion].correct);
+	if (event.target.getAttribute("data-answer-id") == quizList[curQuestion].correct) {
+		contentEl.querySelector("#answerStatus").textContent =  "Correct!";
+		correctAnswers++;
+	}
+	else {
+		answerStatus.textContent =  "Wrong!";
+		// Burn 9 seconds, then call an immediate timer tick to burn another second, for 10 total.
+		timeLeft -= 9;
+		timerTick();
 
-	curQuestion++; // Testing code!
-	loadNextQuestion();
+		// Burned out the clock, the timer function will have ended the test for us.
+		if (timeLeft <= 0)
+			return;
+	}
 
-	answerStatus.textContent = (correct) ? "Correct" : "Wrong";
+	// No more questions to load, we're done!
+	if ((questionNumber >= 10) || (askedQuestions.length >= quizList.length))
+		endTest();
+	else
+		loadNextQuestion();
 }
 
 var beginTest = function() {
-	document.querySelector("#buttonHolder").style.display = "none";
+	var createMe;
 
-	questionEl.style.display = "flex";
+	askedQuestions = []; // Clear asked question storage.
+	questionNumber = 0; // Will increment to 1 immediately.
+	correctAnswers = 0;
+
+	contentEl.innerHTML = "";
+
+	/* ----- Add Header ----- */
+	contentEl.appendChild(document.createElement("h2"));
+	/* ----- Add List for Answers ----- */
+	createMe = document.createElement("ol");
+	createMe.addEventListener("click", answerQuestion);
+	contentEl.appendChild(createMe);
+	/* ----- Add Answer Status ----- */
+	createMe = document.createElement("div");
+	createMe.setAttribute("id", "answerStatus");
+	contentEl.appendChild(createMe);
 
 	loadNextQuestion();
+
+	// Extra second on the timer because we're firing an immediate tick to update the timer display, which will decrement it.
+	timeLeft = 61;
+	timerTick();
+	timer = setInterval(timerTick, 1000);
 }
 
-startButtonEl.addEventListener("click", beginTest);
-answerList.addEventListener("click", answerQuestion);
+var endTest = function() {
+	clearInterval(timer);
+	showGameOver();
+}
+
+var showGameOver = function() {
+	var createMe;
+
+	contentEl.innerHTML = "";
+}
+
+var resetPage = function() {
+	var createMe;
+	contentEl.innerHTML = "";
+
+	/* ----- Add Header ----- */
+	createMe = document.createElement("h1");
+	createMe.textContent = "Javascript Coding Test Challenge!";
+	contentEl.appendChild(createMe);
+
+	/* ----- Add Intro Text ----- */
+	createMe = document.createElement("p");
+	createMe.textContent = "Answer up to 10 questions before time runs out, but beware!  Each wrong answer will cost you 5 seconds!";
+	contentEl.appendChild(createMe);
+
+	/* ----- Add Start Button ----- */
+	createMe = document.createElement("button");
+	createMe.textContent = "Click to Begin!";
+	createMe.className = "large";
+	createMe.setAttribute("type", "button");
+	createMe.addEventListener("click", beginTest);
+	contentEl.appendChild(createMe);
+}
+
+resetPage();
